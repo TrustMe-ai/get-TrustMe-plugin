bdef528a4295bec4ba7864e7588fae4c106c1336
ivin.s <ivin.s@codelynks.com>
2024-11-13 13:48:29 +0530

    update a fail condetion

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,12 @@ export FLAVOUR='github'
 
 echo "======= CLI Version ======="
 # echo "===========${GITHUB_TOKEN}================"
+if [ "$INPUT_HOST" = "dead" ]; then
+    echo "Environment variable STATUS is 'dead'. Exiting with code 1."
+    exit 1
+else
+    echo "Environment variable STATUS is not 'dead'. Exiting with code 0."
+    exit 0
+
+fi
 echo "===========${INPUT_HOST}================"
