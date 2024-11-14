3e3919710d20a6520aaf401b837d685d4a525b72
ivin.s <ivin.s@codelynks.com>
2024-11-14 11:40:24 +0530

    adds dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh -l
 
-set -o errexit
-set -o nounset
-set -o pipefail
+echo "Hello $1"
+time=$(date)
+echo "time=$time" >> $GITHUB_OUTPUT
 
-export FLAVOUR='github'
-
-echo "======= CLI Version ======="
-# echo "===========${GITHUB_TOKEN}================"
-if [ "$INPUT_HOST" = "dead" ]; then
-    echo "Environment variable STATUS is 'dead'. Exiting with code 1."
-    exit 1
-else
-    echo "Environment variable STATUS is not 'dead'. Exiting with code 0."
-    exit 0
-
-fi
-echo "===========${INPUT_HOST}================"
