7c952e5cf2ceed5c8dbd2cc3399814666ee63973
ivin.s <ivin.s@codelynks.com>
2024-11-15 08:18:21 +0530

    small update

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ import subprocess
 
 command = '/app/dist/main'
 result = subprocess.run(command, shell=True, capture_output=True, text=True)
-results = json.loads(result.stdout)
+# results = json.loads(result.stdout)
 
-print(results)
+print(result)
