c9844c96d6911c3879cceab2e6113e3b2c9b7d82
ivin.s <ivin.s@codelynks.com>
2024-11-15 08:23:33 +0530

    update

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ result = subprocess.run(command, shell=True, capture_output=True, text=True)
 # results = json.loads(result.stdout)
 
 print(result.stdout)
+print(result.returncode)
+print(result.stderr)
