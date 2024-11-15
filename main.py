b65bee8c4cdb3c4f07d8d872a2fd226a82e2fb33
ivin.s <ivin.s@codelynks.com>
2024-11-15 07:41:52 +0530

    updates code python script

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('hello world')
+import json
+import subprocess
+
+command = '/app/dist/main'
+result = subprocess.run(command, shell=True, capture_output=True, text=True)
+results = json.loads(result.stdout)
+
+print(results)
