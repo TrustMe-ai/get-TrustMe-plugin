44b35da7902f88ee12b5bcf98b69194e9e71e59e
ivin.s <ivin.s@codelynks.com>
2024-11-15 08:43:32 +0530

    update

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import sys
 import json
 import subprocess
+from pathlib import Path
+
+def list_dirs_and_files(path):
+    return [(entry.name, "Directory" if entry.is_dir() else "File") for entry in Path(path).iterdir()]
+
 
 print(sys.argv)
 
 path = sys.argv[1]
+
+print(list_dirs_and_files(path))
 
 command = f'/app/dist/main {path}'
 print(command)
